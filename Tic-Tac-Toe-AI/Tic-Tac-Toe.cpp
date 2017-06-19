2c0247a91e8c5f7252d2c6c8d0805273c1cd0a1a
dkssudgktp <hellomydir@gmail.com>
2017-06-19 18:02:29 +0900

    add function Tic-Tac-Toe

--- Tic-Tac-Toe-AI/Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe-AI/Tic-Tac-Toe.cpp
@@ -8,13 +8,13 @@ Tic_Tac_Toe::Tic_Tac_Toe() {
 }
 
 void Tic_Tac_Toe::clearBoard(void) {
-	int size = 3;
-	for (int i = 0; i < size; i++) {
+	for (int i = 0,size = 3; i < size; i++) {
 		for (int j = 0; j < size; j++) {
 			board[i][j] = " ";
 		}
 	}
 }
+
 
 void Tic_Tac_Toe::printBoard(void) {
 	cout << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
@@ -22,6 +22,15 @@ void Tic_Tac_Toe::printBoard(void) {
 	cout << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
 	cout << "---------" << endl;
 	cout << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+}
+
+bool Tic_Tac_Toe::isFull(void) {
+	for (int i = 0, size = 3; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (board[i][j] == " ") return false;
+		}
+	}
+	return true;
 }
 
 bool Tic_Tac_Toe::checkWinner(void) {
@@ -47,3 +56,18 @@ bool Tic_Tac_Toe::checkWinner(void) {
 		return false;
 }
 
+int Tic_Tac_Toe::getInput(void) {
+	cout << "input number [1~0]" << endl;
+	return 0;
+}
+void Tic_Tac_Toe::addMove(int getNumber) {
+	if (getNumber == 1 && board[0][0] == " ") board[0][0] = whoTurn;
+	else if(getNumber == 2 && board[0][1] == " ") board[0][1] = whoTurn;
+	else if (getNumber == 3 && board[0][2] == " ") board[0][2] = whoTurn;
+	else if (getNumber == 4 && board[1][0] == " ") board[1][0] = whoTurn;
+	else if (getNumber == 5 && board[1][1] == " ") board[1][1] = whoTurn;
+	else if (getNumber == 6 && board[1][2] == " ") board[1][2] = whoTurn;
+	else if (getNumber == 7 && board[2][0] == " ") board[2][0] = whoTurn;
+	else if (getNumber == 8 && board[2][1] == " ") board[2][1] = whoTurn;
+	else if (getNumber == 9 && board[2][2] == " ") board[2][2] = whoTurn;
+}
--- Tic-Tac-Toe-AI/Tic-Tac-Toe.h
+++ Tic-Tac-Toe-AI/Tic-Tac-Toe.h
@@ -6,13 +6,18 @@ using namespace std;
 class Tic_Tac_Toe{
 private:
 	string board[3][3];
+	
+	string whoTurn = "O"; // first is true
+
 public:
 	Tic_Tac_Toe();
 	bool checkWinner();
+	bool isFull();
 
-
+	void addMove(int);
 	void clearBoard();
 	void printBoard();
+	void aiGaLogic();
 	void gameLogic();
 
 	int getInput();
